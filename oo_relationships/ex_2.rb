ff7d507b3d497920057fbadd501b4652b3d15ad9
djlonboy <djlonboy@gmail.com>
2019-11-07 14:04:21 +0000

    Completed oo_relationships exercise2

---
+++ oo_relationships/ex_2.rb
@@ -0,0 +1,61 @@
+# class User
+#   def initialize(name, bio, age, password)
+#     @name = name
+#     @bio = bio
+#     @age = age
+#     @password = password
+#   end
+#
+#   def authenticate(candidate_password)
+#     return true if candidate_password == @password
+#     false
+#   end
+#
+#   def profile
+#     "#{@name}, born in #{birth_year}: #{@bio}"
+#   end
+#
+#   private
+#
+#   def birth_year
+#     Time.new.year - @age
+#   end
+# end
+
+# Use a forwarding relationship to extract the authentication behaviour
+# from User into another class.
+
+# Draw a diagram first, then do the refactor on the code.
+
+class Authenticate
+  def initialize
+    @user = User.new
+    @password = @user.password
+  end
+
+  def authenticate(candidate_password)
+    return true if candidate_password == @password
+    false
+  end
+end
+
+class User
+  attr_reader :password
+
+  def initialize(name, bio, age, password)
+    @name = name
+    @bio = bio
+    @age = age
+    @password = password
+  end
+
+  def profile
+    "#{@name}, born in #{birth_year}: #{@bio}"
+  end
+
+  private
+
+  def birth_year
+    Time.new.year - @age
+  end
+end
